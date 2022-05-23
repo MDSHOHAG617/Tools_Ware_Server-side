82fe113dc909611fc75d8e7d4f7aa7a511d22994
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-05-24 00:41:19 +0600

    All customers required

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { use } = require("express/lib/router");
+const { is } = require("express/lib/request");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -16,11 +18,27 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     const toolCollection = client.db("tools-ware").collection("tools");
-    // const purchaseCollection = client.db("tools-ware").collection("purchase");
+    const orderCollection = client.db("tools-ware").collection("orders");
+    const userCollection = client.db("tools-ware").collection("users");
 
     app.get("/tool", async (req, res) => {
       const query = {};
@@ -29,11 +47,78 @@ async function run() {
       res.send(tools);
     });
 
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
+
     app.get("/tool/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const purchase = await toolCollection.findOne(query);
-      res.send(purchase);
+      const tool = await toolCollection.findOne(query);
+      res.send(tool);
+    });
+
+    app.get("/order", verifyJWT, async (req, res) => {
+      const customerEmail = req.query.customerEmail;
+      const decodedEmail = req.decoded.email;
+      if (customerEmail === decodedEmail) {
+        const query = { customerEmail: customerEmail };
+        const bookedOrder = await orderCollection.find(query).toArray();
+        return res.send(bookedOrder);
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      // console.log("Auth Header", authorization);
+      // const authorization = req.headers.authorization;
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
     });
   } finally {
   }
